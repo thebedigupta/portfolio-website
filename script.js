738c41f35169955f6a6911ff1a9f04723df52a6c
Bedi Gupta <bedigupta76@gmail.com>
2024-09-07 08:02:13 +0530

    Completed My Website all pages are ready

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const slides = document.querySelectorAll(".stack img");
+let counter = 0;
+const totalSlides = slides.length; // Total number of slides
+
+// Set the initial position of each slide
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const previous = () => {
+  if (counter > 0) {
+    // Check if not at the first slide
+    counter--;
+    slideImage();
+  }
+};
+
+const next = () => {
+  if (counter < totalSlides - 1) {
+    // Check if not at the last slide
+    counter++;
+    slideImage();
+  }
+};
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
